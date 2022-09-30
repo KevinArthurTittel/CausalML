ba2cec25da64b894be4d6aeedad5cd1dd3bfc54b
KevinArthurTittel <114079386+KevinArthurTittel@users.noreply.github.com>
2022-09-30 17:33:57 +0200

    Update Extension Table 4 (DellaVigna and Kaplan, 2007).R

--- Extension Table 4 (DellaVigna and Kaplan, 2007).R
+++ Extension Table 4 (DellaVigna and Kaplan, 2007).R
@@ -23,3 +23,15 @@ library(glmnet)
 
   # Appoint the control variables matrix
     X <- as.matrix(Fox_News_Data[,c(37:49, 139:150, 62:79)])
+    colnames(X) <- c("Population2000", "Population2000Over18", "HighSchoolFraction2000", "SomeCollegeFraction2000",
+                 "CollegeGraduatesFraction2000", "MalesFraction2000", "BlackFraction2000", "HispanicsFraction2000",
+                 "EmploymentFraction2000", "UnemploymentRate2000", "MarriedFraction2000", "Income2000", "ShareUrban2000",
+                 "PopulationDiff19962000", "SomeCollegeFractionDiff19962000", "SomeCollegeFractionDiff19962000",
+                 "CollegeGraduatesDiff19962000", "MalesFractionDiff19962000", "BlackFractionDiff19962000",
+                 "HispanicsFractionDiff19962000", "EmploymentFractionDiff19962000", "UnemploymentRateDiff19962000",
+                 "MarriedFractionDiff19962000", "IncomeDiff19962000", "ShareUrbanDiff19962000", "Decile2NumberChannels",
+                 "Decile3NumberChannels", "Decile4NumberChannels", "Decile5NumberChannels", "Decile6NumberChannels",
+                 "Decile7NumberChannels", "Decile8NumberChannels", "Decile9NumberChannels", "Decile10NumberChannels",
+                 "Decile2PotentialSubscribers", "Decile3PotentialSubscribers", "Decile4PotentialSubscribers",
+                 "Decile5PotentialSubscribers", "Decile6PotentialSubscribers", "Decile7PotentialSubscribers",
+                 "Decile8PotentialSubscribers", "Decile9PotentialSubscribers", "Decile10PotentialSubscribers")
