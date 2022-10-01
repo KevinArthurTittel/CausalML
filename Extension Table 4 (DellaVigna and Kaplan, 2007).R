43207f869b563a8b423a4202a553fb452a31886e
KevinArthurTittel <114079386+KevinArthurTittel@users.noreply.github.com>
2022-10-01 22:10:12 +0200

    Update Extension Table 4 (DellaVigna and Kaplan, 2007).R

--- Extension Table 4 (DellaVigna and Kaplan, 2007).R
+++ Extension Table 4 (DellaVigna and Kaplan, 2007).R
@@ -23,30 +23,33 @@ library(glmnet)
 
   # Appoint the control variables matrix
     demographic.controls.2000 <- Fox_News_Data[,c(37:49)]
-    colnames(demographic.controls.2000) <- c("Population2000", "Population2000Over18", "HighSchoolFraction2000", "SomeCollegeFraction2000",
-                                         "CollegeGraduatesFraction2000", "MalesFraction2000", "BlackFraction2000", "HispanicsFraction2000",
-                                         "EmploymentFraction2000", "UnemploymentRate2000", "MarriedFraction2000", "Income2000", "ShareUrban2000")
+    colnames(demographic.controls.2000) <- c("Population 2000", "Population (over 18) 2000", "Share with high school 2000", "Share with some college 2000",
+                                         "Share with college degree 2000", "Share male 2000", "Share African Americans 2000", "Share Hispanics 2000",
+                                         "Employment rate 2000", "Unemployment rate 2000", "Share married 2000", "Median income 2000", "Share urban 2000")
 
     demographic.controls.Diff19962000 <- Fox_News_Data[,c(139:150)]
-    colnames(demographic.controls.Diff19962000) <- c("PopulationDiff19962000", "SomeCollegeFractionDiff19962000", "SomeCollegeFractionDiff19962000",
-                                                 "CollegeGraduatesDiff19962000", "MalesFractionDiff19962000", "BlackFractionDiff19962000",
-                                                 "HispanicsFractionDiff19962000", "EmploymentFractionDiff19962000", "UnemploymentRateDiff19962000",
-                                                 "MarriedFractionDiff19962000", "IncomeDiff19962000", "ShareUrbanDiff19962000")
+    colnames(demographic.controls.Diff19962000) <- c("Population, Diff. btwn. 2000 and 1996", "Share with high school, Diff. btwn. 2000 and 1996", "Share with some college, Diff. btwn. 2000 and 1996",
+                                                 "Share with college degree, Diff. btwn. 2000 and 1996", "Share male, Diff. btwn. 2000 and 1996", "Share African Americans, Diff. btwn. 2000 and 1996",
+                                                 "Share Hispanics, Diff. btwn. 2000 and 1996", "Employment rate, Diff. btwn. 2000 and 1996", "Unemployment rate, Diff. btwn. 2000 and 1996",
+                                                 "Share married, Diff. btwn. 2000 and 1996", "Median income, Diff. btwn. 2000 and 1996", "Share urban, Diff. btwn. 2000 and 1996")
 
     Decile1NumberChannels <- as.integer((rowSums(Fox_News_Data[,c(62:70)]) == 0))
     Decile1PotentialSubscribers <- as.integer(rowSums(Fox_News_Data[,c(71:79)]) == 0)
 
     cable.controls <- cbind(Decile1NumberChannels, Fox_News_Data[,c(62:70)], Decile1PotentialSubscribers, Fox_News_Data[,c(71:79)])
-    colnames(cable.controls) <- c("Decile1NumberChannels, Decile2NumberChannels", "Decile3NumberChannels", "Decile4NumberChannels", 
-                              "Decile5NumberChannels", "Decile6NumberChannels", "Decile7NumberChannels", "Decile8NumberChannels", 
-                              "Decile9NumberChannels", "Decile10NumberChannels", "Decile1PotentialSubscribers", "Decile2PotentialSubscribers", 
-                              "Decile3PotentialSubscribers", "Decile4PotentialSubscribers", "Decile5PotentialSubscribers", 
-                              "Decile6PotentialSubscribers", "Decile7PotentialSubscribers", "Decile8PotentialSubscribers", "Decile9PotentialSubscribers", "Decile10PotentialSubscribers")
+    colnames(cable.controls) <- c("Decile 1 no. cable channels", "Decile 2 no. cable channels", "Decile 3 no. cable channels", 
+                                  "Decile 4 no. cable channels", "Decile 5 no. cable channels", "Decile 6 no. cable channels", 
+                                  "Decile 7 no. cable channels", "Decile 8 no. cable channels", "Decile 9 no. cable channels", 
+                                  "Decile 10 no. cable channels", "Decile 1 no. potential subscribers", "Decile 2 no. potential subscribers", 
+                                  "Decile 3 no. potential subscribers", "Decile 4 no. potential subscribers", "Decile 5 no. potential subscribers", 
+                                  "Decile 6 no. potential subscribers", "Decile 7 no. potential subscribers", "Decile 8 no. potential subscribers", 
+                                  "Decile 9 no. potential subscribers", "Decile 10 no. potential subscribers")
+
 
     X <- as.matrix(cbind(demographic.controls.2000, demographic.controls.Diff19962000, cable.controls))
 
-    remaining.variables <- Fox_News_Data[,c(14,59)]
-    colnames(remaining.variables) <- c("NumberCableChannels", "NumberPotentialSubscribers")
+    remaining.variables <- Fox_News_Data[,c(14,59, 204, 205)]
+    colnames(remaining.variables) <- c("NumberCableChannels", "NumberPotentialSubscribers", "Swing district", "Republican district")
 
   # Clean the data
     indices <- (Fox_News_Data$sample12000 == 1)
@@ -115,6 +118,9 @@ run_method = function(numtrees, index, lambdas, boolean.plot, boolean.lambdas) {
       # Assessing GRF fit and heterogeneity using the Best Linear Predictor Approach [BLP]
         GRF.BLP <- test_calibration(GRF)
     
+      # Test of heterogeneity using Differential ATE along each and every variable
+        
+        
       # Test of heterogeneity using Differential ATE
         GRF.ATE.charact <- average_treatment_effect(GRF, target.sample = "all", subset = (characteristic == 1))
         GRF.ATE.not.charact <- average_treatment_effect(GRF, target.sample = "all", subset = !(characteristic == 1))
