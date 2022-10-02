cd1c23a42dd58cb303a3f7b9e143609861aa8655
KevinArthurTittel <114079386+KevinArthurTittel@users.noreply.github.com>
2022-10-02 12:57:45 +0200

    Update Data Preparation Microfinance Project (Field et alum 2013).R

--- Data Preparation Microfinance Project (Field et alum 2013).R
+++ Data Preparation Microfinance Project (Field et alum 2013).R
@@ -7,7 +7,7 @@
     W <- as.vector(W)
 
   # Create a numerical vector of the character group name vector
-    # loangroups <- as.numeric(factor(Grace_Period_Data$sec_group_name))
+    loangroups <- as.numeric(factor(Grace_Period_Data$sec_group_name))
 
   # Standardize the continuous variables
     Grace_Period_Data$Years_Education_C <- scale(Grace_Period_Data$Years_Education_C)
@@ -16,8 +16,8 @@
     Grace_Period_Data$HH_Size_C <- scale(Grace_Period_Data$HH_Size_C)
 
   # Appoint the control variables matrix
-    X <- Grace_Period_Data[,c(6,8:18)]
-    colnames(X) <- c("Stratification.Dummies", "Age", "Married", "Literate", "Muslim", "HH.Size", "Years.Education", "Shock", "Has.Business",
+    X <- Grace_Period_Data[,c(2,6,8:18)]
+    colnames(X) <- c("Loan.Officer", "Stratification", "Age", "Married", "Literate", "Muslim", "HH.Size", "Years.Education", "Shock", "Has.Business",
                  "Financial.Control", "Home.Owner", "No.Drain")
                  
   # Create loan group dummies (as in original analysis) to be added to control variables matrix
@@ -29,27 +29,6 @@
     loansize6 <- as.integer(c(Grace_Period_Data$sec_loanamount == 10000))
     loansizematrix <- cbind(loansize1, loansize2, loansize3, loansize4,
                         loansize5, loansize6)
-    loangroups <- Grace_Period_Data$sec_loanamount
-    
-  # Create stratification dummies (fixed effects) to be added to control variables matrix
-    stratifgroup1 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 1))
-    stratifgroup2 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 2))
-    stratifgroup3 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 3))
-    stratifgroup4 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 4))
-    stratifgroup5 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 5))
-    stratifgroup6 <- as.integer(c(Grace_Period_Data$Stratification_Dummies == 6))
-    stratifmatrix <- cbind(stratifgroup1, stratifgroup2, stratifgroup3, 
-                       stratifgroup4, stratifgroup5, stratifgroup6)
-
-  # Create loan officer dummies (fixed effects) to be added to control variables matrix
-    loanofficergroup1 <- as.integer(c(Grace_Period_Data$sec_loan_officer == 1))
-    loanofficergroup2 <- as.integer(c(Grace_Period_Data$sec_loan_officer == 3))
-    loanofficergroup3 <- as.integer(c(Grace_Period_Data$sec_loan_officer == 4))
-    loanofficergroup4 <- as.integer(c(Grace_Period_Data$sec_loan_officer == 7))
-    loanofficergroup5 <- as.integer(c(Grace_Period_Data$sec_loan_officer == 8))
-    loanofficermatrix <- cbind(loanofficergroup1, loanofficergroup2, loanofficergroup3, 
-                           loanofficergroup4, loanofficergroup5)
 
   # Combine all the control covariates in one large matrix
-    # X <- as.matrix(cbind(X, stratifmatrix, loansizematrix, loanofficermatrix))
-    X <- as.matrix(X)
+    X <- as.matrix(cbind(X, loansizematrix))
