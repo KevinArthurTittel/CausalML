87d807555bf402714654d8daeda1fe8b1b23cc79
KevinArthurTittel <114079386+KevinArthurTittel@users.noreply.github.com>
2022-10-11 12:13:33 +0200

    Update Extending Table 4 (Arceneaux, 2006).R

--- Extending Table 4 (Arceneaux, 2006).R
+++ Extending Table 4 (Arceneaux, 2006).R
@@ -1,3 +1,13 @@
+library(grf)
+library(sandwich)
+library(lmtest)
+library(Hmisc)
+library(ggplot2)
+library(standardize)
+library(foreign)
+library(haven)
+library(glmnet)
+library(stats4)
 set.seed(123)
 
 # Import and prepare Voting Study data set (Arceneaux et al., 2006)
@@ -24,6 +34,7 @@ for (i in 1:length(county.clusters)) {
   dummies.county.clusters[i,toString(county.clusters[i])] <- 1
 }
 
+# Synthetic treatment assignment
 tau <- -1* (X$vote00 / (2 + (100/X$age)))
 R <- rbinom(n = length(tau), size = 1, prob = abs(tau))
 Y0 <- matrix(0, nrow = length(Y), ncol = 1)
@@ -39,12 +50,36 @@ Ynew[(W == 0)] <- Y0[(W == 0)]
 Ynew[(W == 1)] <- Y1[(W == 1)]
 
 # Clean the data
-treatment.group <- (voting.study.data$treat_real == 1)
-control.group <- (voting.study.data$treat_real == 0)
+treatment.group <- sample(which(voting.study.data$treat_real == 1), size = length(which((voting.study.data$treat_real == 1))), replace = FALSE)
+control.group <- which(voting.study.data$treat_real == 0)
 subsample.control.group <- sample(control.group, size = 89958, replace = FALSE)
+X.treatment.group <- X[treatment.group,]
+X.control.group <- X[subsample.control.group,]
+W.treatment.group <- W[treatment.group]
+W.control.group <- W[subsample.control.group]
+Ynew.treatment.group <- Ynew[treatment.group]
+Ynew.control.group <- Ynew[subsample.control.group]
+county.clusters.treatment.group <- county.clusters[treatment.group]
+county.clusters.control.group <- county.clusters[subsample.control.group]
+dummies.county.clusters.treatment.group <- dummies.county.clusters[treatment.group,]
+dummies.county.clusters.control.group <- dummies.county.clusters[subsample.control.group,]
 
-# Synthetic treatment assignment
-
+# Appoint training and test sets
+X.train <- rbind(X.treatment.group[1:40000,], X.control.group[1:60000,])
+X.test <- rbind(X.treatment.group[40001:50000,], X.control.group[60001:75000,])
+X.holdout <- rbind(X.treatment.group[50001:nrow(X.treatment.group),], X.control.group[75001:nrow(X.control.group),])
+W.train <- c(W.treatment.group[1:40000], W.control.group[1:60000])
+W.test <- c(W.treatment.group[40001:50000], W.control.group[60001:75000])
+W.holdout <- c(W.treatment.group[50001:length(W.treatment.group)], W.control.group[75001:length(W.control.group)])
+Ynew.train <- c(Ynew.treatment.group[1:40000], Ynew.control.group[1:60000])
+Ynew.test <- c(Ynew.treatment.group[40001:50000], Ynew.control.group[60001:75000])
+Ynew.holdout <- c(Ynew.treatment.group[50001:length(Ynew.treatment.group)], Ynew.control.group[75001:length(Ynew.control.group)])
+county.clusters.train <- c(county.clusters.treatment.group[1:40000], county.clusters.control.group[1:60000])
+county.clusters.test <- c(county.clusters.treatment.group[40001:50000], county.clusters.control.group[60001:75000])
+county.clusters.holdout <- c(county.clusters.treatment.group[50001:length(county.clusters.treatment.group)], county.clusters.control.group[75001:length(county.clusters.control.group)])
+dummies.county.clusters.train <- rbind(dummies.county.clusters.treatment.group[1:40000,], dummies.county.clusters.control.group[1:60000,])
+dummies.county.clusters.test <- rbind(dummies.county.clusters.treatment.group[40001:50000,], dummies.county.clusters.control.group[60001:75000,])
+dummies.county.clusters.holdout <- rbind(dummies.county.clusters.treatment.group[50001:nrow(dummies.county.clusters.treatment.group),], dummies.county.clusters.control.group[75001:nrow(dummies.county.clusters.control.group),])
 
 # Initialize parameters
 numtrees <- 2000 # Set to 1000 or 5000 to perform sensitivity analysis.
@@ -55,7 +90,6 @@ filename.plot.GRF.CATE <- "GRF CATE .pdf"
 filename.plot.CR.GRF.CATE <- "CR.GRF CATE .pdf"
 filename.plot.LLCF.CATE <- "LLCF CATE .pdf"
 
-
 # Estimation procedure
 
 ###########################
@@ -63,12 +97,12 @@ filename.plot.LLCF.CATE <- "LLCF CATE .pdf"
 ###########################
 
 # For GRF we create the X matrix including district-specific dummies
-current.X <- cbind(X, dummies.county.clusters)
+current.X <- cbind(X.train, dummies.county.clusters.train)
 
 # Grow preliminary forests for (W, X) and (Y, X) separately
-forest.W <- regression_forest(current.X, W, num.trees = numtrees, honesty = TRUE, tune.parameters = "all")
+forest.W <- regression_forest(current.X, W.train, num.trees = numtrees, honesty = TRUE, tune.parameters = "all")
 W.hat <- predict(forest.W)$predictions
-forest.Y <- regression_forest(current.X, Y, num.trees = numtrees, honesty = TRUE, tune.parameters = "all")
+forest.Y <- regression_forest(current.X, Y.train, num.trees = numtrees, honesty = TRUE, tune.parameters = "all")
 Y.hat <- predict(forest.Y)$predictions
 
 # Compute the variable importance
@@ -77,10 +111,10 @@ GRF.varimp.ordered <- order(GRF.varimp)
 GRF.mostimportant <- colnames(X)[(GRF.varimp.ordered[1:4])] # 4 most important variables for splitting
 
 # Select variables to include using preliminary GRF
-prelim.GRF <- causal_forest(current.X, Y, W, Y.hat = Y.hat, W.hat = W.hat, num.trees = numtrees, honesty = TRUE)
+prelim.GRF <- causal_forest(current.X, Y.train, W.train, Y.hat = Y.hat, W.hat = W.hat, num.trees = numtrees, honesty = TRUE)
 prelim.GRF.varimp <- variable_importance(prelim.GRF)
 selected.vars <- which(prelim.GRF.varimp / mean(prelim.GRF.varimp) > 0.2)
 
 # Implement GRF
-GRF <- causal_forest(current.X[,selected.vars], Y, W, Y.hat = Y.hat, W.hat = W.hat, num.trees = numtrees, 
+GRF <- causal_forest(current.X[,selected.vars], Y.train, W.train, Y.hat = Y.hat, W.hat = W.hat, num.trees = numtrees, 
                      honesty = TRUE, tune.parameters = "all")
